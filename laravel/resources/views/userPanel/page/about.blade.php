6971498aad317847a8b3badb59c620439482a600
DESKTOP-99VFG60\Shafa Tathya <shafatathya@gmail.com>
2021-01-03 22:11:22 +0700

    nambahin about dikit

--- laravel/resources/views/userPanel/page/about.blade.php
+++ laravel/resources/views/userPanel/page/about.blade.php
@@ -9,15 +9,12 @@
         </div>
         <div class="col-md-6">
             <p class="h2 font-weight-bold d-inline">About Us</p>
+            <p class="p text-justify">KIDDY BANK adalah platform edukasi finansial untuk anak yang dibentuk pada tanggal 24 September 2020. 
+            Tentu saja KIDDY BANK tidak akan berdiri dengan sendiri tanpa bantuan dari orang-orang di balik layar. Pengembangan KIDDY BANK melibatkan
+            lima orang, yaitu Shafa Tathya Larasati, Devi Silviani, Irene Callista Harefa, Meutia Aulia Fitri, dan Rifki Muhamad Aditama. &#128513;</p>
             <p class="p text-justify">Direktur Jenderal Pendidikan Anak Usia Dini dan Pendidikan Masyarakat, Harris Iskandar, memaparkan bahwa pendidikan literasi finansial di Indonesia masih menjadi sesuatu yang sangat jarang dilakukan. Edukasi literasi finansial pada anak bukan sekadar pengenalan uang, namun sebuah konsep tentang pengenalan pengelolaan keuangan secara bijak dan mampu mengontrol pengeluaran keuangan dengan membedakan mana yang menjadi kebutuhan dan mana yang hanya keinginan, sehingga kelak dapat memiliki perencanaan keuangan yang baik.</p>
-                <p class="p text-justify"> Hal ini yang mendasari penerapan teknologi sebagai sarana edukasi bagi literasi finansial anak yang dikemas dalam website KIDDY BANK dengan harapan dapat membantu anak maupun orang tua terkait pendidikan literasi finansial sejak dini.KIDDY BANK sendiri dibentuk pada tanggal 24 September 2020.
+            <p class="p text-justify">Hal ini yang mendasari penerapan teknologi sebagai sarana edukasi bagi literasi finansial anak yang dikemas dalam website KIDDY BANK dengan harapan dapat membantu anak maupun orang tua terkait pendidikan literasi finansial sejak dini.
             </p>
-            <h3><center>TEAM</h3>
-        <li>Shafa Tathya Larasati : Project Manager</li>
-        <li>Devi Silviani : System Analyst</li>
-        <li>Irene Callista Harefa : Database Administrator</li>
-        <li>Meutia Aulia Fitri : UI/UX Designer</li>
-        <li>Rifki Muhamad Aditama : Front-end Developer
         </div>
     </div>
 </div>
